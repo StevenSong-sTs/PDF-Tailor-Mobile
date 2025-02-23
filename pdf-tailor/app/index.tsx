44089d4b836b62572344b698ad3e920e2d751359
StevenSong-sTs <songxincheng124@gmail.com>
2025-02-23 10:12:59 -0800

    build an simple example app

--- pdf-tailor/app/index.tsx
+++ pdf-tailor/app/index.tsx
@@ -1,15 +1,32 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { View } from "react-native";
+import { Button, ButtonText } from "@/components/ui/button";
+import { Text } from "@/components/ui/text";
+import { Heart } from "lucide-react-native";
 
 export default function Index() {
+  const [count, setCount] = useState(0);
+
   return (
     <View
       style={{
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        gap: 20,
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button
+        size="lg"
+        variant="solid"
+        action="primary"
+        onPress={() => setCount(count + 1)}
+      >
+        <Heart size={24} color="white" />
+        <ButtonText>Hello World</ButtonText>
+      </Button>
+
+      <Text size="lg">Button clicked: {count} times</Text>
     </View>
   );
 }
